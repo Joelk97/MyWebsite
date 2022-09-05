7e895efc3b345648690e547e4eea4a6e7ee6d40b
Joelk97 <93705438+Joelk97@users.noreply.github.com>
2022-09-05 11:34:22 +0200

    update

--- js/header_footer.js
+++ js/header_footer.js
@@ -15,3 +15,23 @@ class myHeader extends HTMLElement {
     }
 }
 customElements.define('my-header', myHeader)
+
+class myFooter extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <footer class="main-footer">
+            <div class="social-icons">
+                <ul>
+                    <li><a href="https://github.com/Joelk97"><img src="img/icon-github.png"></a></li>
+                    <li><a href="https://www.linkedin.com/in/joel-k%C3%BChl-276297192/"><img src="img/icon-linkedin.png"></a></li>
+                    <li><a href="https://www.strava.com/athletes/4548209"><img src="img/icon-strava.png"></a></li>
+                </ul>
+            </div>
+            <div class="copyright">
+                <p>&copy; 2022 Joel Kuehl</p>
+            </div>
+        </footer>
+        `
+    }
+}
+customElements.define('my-footer', myFooter)
