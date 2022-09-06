f7602b03fe21668b6e873326d1e353904d4777c3
Joelk97 <93705438+Joelk97@users.noreply.github.com>
2022-09-06 08:07:49 +0200

    update

--- js/header_footer.js
+++ js/header_footer.js
@@ -25,6 +25,7 @@ class myFooter extends HTMLElement {
                     <li><a href="https://github.com/Joelk97"><img src="img/icon-github.png"></a></li>
                     <li><a href="https://www.linkedin.com/in/joel-k%C3%BChl-276297192/"><img src="img/icon-linkedin.png"></a></li>
                     <li><a href="https://www.strava.com/athletes/4548209"><img src="img/icon-strava.png"></a></li>
+                    <li><a href="mailto:kuehljoel@gmail.com"><img src="img/icon-email.png"></a></li>
                 </ul>
             </div>
             <div class="copyright">
