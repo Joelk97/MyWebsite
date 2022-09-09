95044d354e70f385748cf324672f1b6f48f78396
Joelk97 <93705438+Joelk97@users.noreply.github.com>
2022-09-09 15:19:04 +0200

    Update header_footer.js

--- js/header_footer.js
+++ js/header_footer.js
@@ -6,7 +6,7 @@ class myHeader extends HTMLElement {
                 <ul>
                     <li><a href="index.html">Home</a></li>
                     <li><a href="about.html">About</a></li>
-                    <li><a href="">Projects</a></li>
+                    <li><a href="projects.html">Projects</a></li>
                     <li><a href="">Websites</a></li>
                 </ul>
             </nav>
